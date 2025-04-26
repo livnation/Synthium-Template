c040a0a6dd1d3564adb1a252db6fae9b2293330f
cipher <151690662+cipherischill@users.noreply.github.com>
2025-04-26 11:41:37 -0400

    rigpatcher

---
+++ Synthium/Backend/Patches/RigPatches.cs
@@ -0,0 +1,22 @@
+using HarmonyLib;
+
+namespace Synthium.Backend.Patches
+{
+    [HarmonyPatch(typeof(VRRig), "OnDisable")]
+    public class RigPatch
+    {
+        public static bool Prefix(VRRig __instance)
+        {
+            return !(__instance == GorillaTagger.Instance.offlineVRRig);
+        }
+    }
+
+    [HarmonyPatch(typeof(VRRigJobManager), "DeregisterVRRig")]
+    public static class RigPatch2
+    {
+        public static bool Prefix(VRRigJobManager __instance, VRRig rig)
+        {
+            return !(__instance == GorillaTagger.Instance.offlineVRRig);
+        }
+    }
+}
